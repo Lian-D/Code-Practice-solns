471d872430ac7f7819830381c4d5bacb249aee60
Lian Duan <LianDuan64@gmail.com>
2022-06-22 16:26:16 -0700

    Create is Subsequence.ts

---
+++ LeetCode/Easy/is Subsequence.ts
@@ -0,0 +1,16 @@
+//Runtime: 65 ms, faster than 97.99% of TypeScript online submissions for Is Subsequence.
+//Memory Usage: 43 MB, less than 67.11% of TypeScript online submissions for Is Subsequence.
+
+function isSubsequence(s: string, t: string): boolean {
+    let sIndex = 0;
+    for (let i = 0; i < t.length; i++) {
+        if (s.charAt(sIndex) == t.charAt(i)) {
+            sIndex++;
+        }
+    }
+    if (sIndex == s.length) {
+        return true
+    } else {
+        return false;
+    }
+};

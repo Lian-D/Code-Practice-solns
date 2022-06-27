cb55763400ff556ac02bda9b07a5001c607659da
Lian Duan <LianDuan64@gmail.com>
2022-06-27 13:53:12 -0700

    Update mergeTwoSortedLists.ts

--- LeetCode/Easy/mergeTwoSortedLists.ts
+++ LeetCode/Easy/mergeTwoSortedLists.ts
@@ -1,31 +1,26 @@
-// Runtime: 104 ms, faster than 19.46% of TypeScript online submissions for Merge Two Sorted Lists.
-//     Memory Usage: 40.7 MB, less than 84.24% of TypeScript online submissions for Merge Two Sorted Lists.
-function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    let temporaryNode: ListNode = new ListNode(0);
-    let currNode: ListNode = temporaryNode
+//Runtime: 88 ms, faster than 78.38% of TypeScript online submissions for Merge Two Sorted Lists.
+//Memory Usage: 45 MB, less than 43.98% of TypeScript online submissions for Merge Two Sorted Lists.
 
-    while (l1 && l2) {
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    let returnNode: ListNode = new ListNode(0);
+    let currNode = returnNode;
 
-        if (l1.val < l2.val) {
-            currNode.next = l1;
-            l1 = l1.next;
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            currNode.next = list1;
+            list1 = list1.next;
         } else {
-            currNode.next = l2;
-            l2 = l2.next
+            currNode.next = list2;
+            list2 = list2.next;
         }
-
         currNode = currNode.next;
+        
     }
-
-    if (l1 != null) {
-        currNode.next = l1;
-        l1 = l1.next;
-
+    if (list1 != null) {
+        currNode.next = list1;
     }
-    if (l2 != null) {
-        currNode.next = l2;
-        l2 = l2.next;
+    if (list2 != null) {
+        currNode.next = list2;
     }
-
-    return temporaryNode.next;
+    return returnNode.next;
 };

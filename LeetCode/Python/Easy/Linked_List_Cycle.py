6ab93ec1f4a9e57330d4031f991314acbc6bb617
lian duan <LianDuan64@gmail.com>
2024-11-13 18:11:16 -0800

    squares of a sorted array python

--- LeetCode/Python/Easy/Linked_List_Cycle.py
+++ LeetCode/Python/Easy/Linked_List_Cycle.py
@@ -1,7 +1,8 @@
- class ListNode(object):
-     def __init__(self, x):
-         self.val = x
-         self.next = None
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
 class Solution(object):
     def hasCycle(self, head):

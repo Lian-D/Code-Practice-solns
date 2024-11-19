62e7dc285556fd625005bac230b9c5344eb42eaf
lian duan <LianDuan64@gmail.com>
2024-11-18 20:06:56 -0800

    Find N Unique Integer Sum

--- LeetCode/Python/Easy/Find_N_Unique_Integer_Sum_Zero.py
+++ LeetCode/Python/Easy/Find_N_Unique_Integer_Sum_Zero.py
@@ -1,6 +1,19 @@
 class Solution(object):
     def sumZero(self, n):
-        """
-        :type n: int
-        :rtype: List[int]
-        """
+        end = []
+        if n % 2 == 0:
+            index = n/2
+            for i in range(index):
+                end.append(i+1)
+                end.append(-abs(i+1))
+            return end
+        else:
+            index = n//2
+            for i in range(index):
+                end.append(i+1)
+                end.append(-abs(i+1))
+            end.append(0)
+            return end
+
+solution = Solution
+print(solution.sumZero(solution, 5))
